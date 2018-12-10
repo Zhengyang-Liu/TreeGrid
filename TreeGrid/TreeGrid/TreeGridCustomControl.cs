b1a1d473af29db0fd286cb9d0c7f1b1aed91f0af
Zhengyang Liu <liuzhengyang183@gmail.com>
2018-12-10 12:00:37 +0800

    1nit

---
+++ TreeGrid/TreeGrid/TreeGridCustomControl.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeGrid
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:TreeGrid"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:TreeGrid;assembly=TreeGrid"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:TreeGridCustomControl/>
+    ///
+    /// </summary>
+    public class TreeGridCustomControl : TreeView
+    {
+        static TreeGridCustomControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeGridCustomControl), new FrameworkPropertyMetadata(typeof(TreeGridCustomControl)));
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TreeListViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is TreeListViewItem;
+        }
+
+        #region Public Properties
+
+        /// <summary> GridViewColumn List</summary>
+        public GridViewColumnCollection Columns
+        {
+            get
+            {
+                if (_columns == null)
+                {
+                    _columns = new GridViewColumnCollection();
+                }
+
+                return _columns;
+            }
+        }
+
+        private GridViewColumnCollection _columns;
+
+        #endregion
+    }
+
+    public class TreeListViewItem : TreeViewItem
+    {
+        /// <summary>
+        /// Item's hierarchy in the tree
+        /// </summary>
+        public int Level
+        {
+            get
+            {
+                if (_level == -1)
+                {
+                    TreeListViewItem parent = ItemsControlFromItemContainer(this) as TreeListViewItem;
+                    _level = (parent != null) ? parent.Level + 1 : 0;
+                }
+                return _level;
+            }
+        }
+
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TreeListViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is TreeListViewItem;
+        }
+
+        private int _level = -1;
+    }
+}
